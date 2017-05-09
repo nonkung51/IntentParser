e98fbe8994776acf7185da17039e0e72064ae69c
nonkung51 <non_thakon@hotmail.com>
2017-05-09 14:56:02 +0700

    Change example a little bit

--- examples/favourite-music-example.py
+++ examples/favourite-music-example.py
@@ -1,23 +1,23 @@
 import intentparser
 
-if __name__ == "__main__":
-    intent = intentparser.intentParser({
-        'description' : {
-                        "type" : 'FavMusicIntent',
-                        "args" : [(1, "musics_types")],
-                        "keyword" : [
-                        (0, "musics_keyword"),
-                        (1, "musics_types")
-                        ]},
-        'musics_keyword' : ['is', 'are', 'music', 'favourite', 'genre'],
-        'musics_types' : [
-            "pop",
-            "rock",
-            "jazz",
-            "country",
-            "reggae"
-            ]
-    })
-    intent.teachWords(["I love Reggae music.", "Rock is my favourite.", "I love Country music genre."])
-    print(intent.getResult("I love Rock music."))
-    print(intent.getResult("Jazz is my favourite."))
+intent = intentparser.intentParser({
+    'description' : {
+                    "type" : 'FavMusicIntent',
+                    "args" : [(1, "musics_types")],
+                    "keyword" : [
+                    (0, "musics_keyword"),
+                    (1, "musics_types")
+                    ]},
+    'musics_keyword' : ['is', 'are', 'music', 'favourite', 'genre'],
+    'musics_types' : [
+        "pop",
+        "rock",
+        "jazz",
+        "country",
+        "reggae"
+        ]
+})
+intent.teachWords(["I love Reggae music.", "Rock is my favourite.", "I love Country music genre."])
+
+print(intent.getResult("I love Rock music."))
+print(intent.getResult("Jazz is my favourite."))
--- examples/weather-example-gensim.py
+++ examples/weather-example-gensim.py
@@ -1,26 +1,26 @@
 import intentparser
 
-if __name__ == "__main__":
-    intent = intentparser.intentParser({
-    # 1 for optinal 2 for regex
-        'description' : {
-                        "type" : 'WeatherIntent',
-                        "args" : [(2,"location"), (1, "weather_types")],
-                        "keyword" : [
-                        (0, "weather_keyword"),
-                        (1, "weather_types"),
-                        (2, "location")
-                        ]},
-        'weather_keyword' : ['is', 'are', 'weather', 'sleet', 'rain', 'humid'],
-        'weather_types' : [
-            "snow",
-            "rain",
-            "wind",
-            "sleet",
-            "sun"
-            ],
-        'location' : ' in (?P<location>.*)'
-    })
-    intent.teachWords(["Weather in Bangkok?", "Good weather in Canada?", "Is it rain in North California?"], typeOfSim='gensim')
-    print(intent.getResult("Rain in Khon Kaen?"))
-    print(intent.getResult("Is it rain in California?"))
+intent = intentparser.intentParser({
+# 1 for optinal 2 for regex
+    'description' : {
+                    "type" : 'WeatherIntent',
+                    "args" : [(2,"location"), (1, "weather_types")],
+                    "keyword" : [
+                    (0, "weather_keyword"),
+                    (1, "weather_types"),
+                    (2, "location")
+                    ]},
+    'weather_keyword' : ['is', 'are', 'weather', 'sleet', 'rain', 'humid'],
+    'weather_types' : [
+        "snow",
+        "rain",
+        "wind",
+        "sleet",
+        "sun"
+        ],
+    'location' : ' in (?P<location>.*)'
+})
+intent.teachWords(["Weather in Bangkok?", "Good weather in Canada?", "Is it rain in North California?"], typeOfSim='gensim')
+
+print(intent.getResult("Rain in Khon Kaen?"))
+print(intent.getResult("Is it rain in California?"))
