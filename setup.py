aab8202a6e8c7762ef7ae7566235d6301a681484
nonkung51 <non_thakon@hotmail.com>
2017-05-14 21:05:14 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import find_packages
 
 
 setup(name='intentparser',
-      version='1.5.9',
+      version='1.5.14',
       description='Use to parse an intent easily.',
       author='Nonthakon Jitchiranant',
       author_email='non_thakon@hotmail.com',
       url='https://github.com/nonkung51/IntentParser',
       download_url='https://github.com/nonkung51/IntentParser/archive/master.zip',
       license='Apache-2.0',
-      install_requires=['nltk', 'gensim'],
+      install_requires=['gensim'],
       packages=find_packages())
